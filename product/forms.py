1f1fc28a091e7707751acd431fc874e028a81e0e
Shiva <shivakumarsunderamurthy@gmail.com>
2021-08-22 19:47:32 +0800

    Some changes

--- product/forms.py
+++ product/forms.py
@@ -5,6 +5,7 @@ from .models import Product, Comment
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import User
+from django.forms import PasswordInput, EmailInput, TextInput
 
 class ProductForm(ModelForm):
     class Meta:
@@ -16,10 +17,16 @@ class ProductForm(ModelForm):
 class RegisterUserForm(UserCreationForm):
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
-
+    password1 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter a password', }))
+    password2 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder':'Your Name', }),
+            'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email adress'}),
+
+            }
     # password1 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter a password', }))
     # password2 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'}))
 
