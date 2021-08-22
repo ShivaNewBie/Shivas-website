f083e40d31a81c5fa6aef6e0c8b82c88ec1c51a5
Shiva <shivakumarsunderamurthy@gmail.com>
2021-08-22 18:10:05 +0800

    lols

--- website/settings.py
+++ website/settings.py
@@ -116,11 +116,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-MEDIA_ROOT = 'static/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = 'static/media/'
